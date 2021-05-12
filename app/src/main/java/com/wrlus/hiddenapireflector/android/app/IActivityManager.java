d54d36fe578e319b096cd7cfc10b3d1d63eb413a
wrlu <wrlus@outlook.com>
2021-05-12 11:07:00 +0800

    Fix IActivityManager

--- app/src/main/java/com/wrlus/hiddenapireflector/android/app/IActivityManager.java
+++ app/src/main/java/com/wrlus/hiddenapireflector/android/app/IActivityManager.java
@@ -1,15 +1,17 @@
 package com.wrlus.hiddenapireflector.android.app;
 
+import android.annotation.SuppressLint;
 import android.os.IBinder;
 
 import java.lang.reflect.Method;
 
 public class IActivityManager {
 
+    @SuppressLint("BlockedPrivateApi")
     public static Object getContentProviderExternal(Object am, String name, int userId,
                                                     IBinder token, String tag) {
         try {
-            Method method = android.app.ActivityManager.class
+            Method method = Class.forName("android.app.IActivityManager")
                     .getDeclaredMethod("getContentProviderExternal",
                             String.class, int.class, IBinder.class, String.class);
             return method.invoke(am, name, userId, token, tag);
@@ -19,10 +21,11 @@ public class IActivityManager {
         return null;
     }
 
+    @SuppressLint("BlockedPrivateApi")
     public static void removeContentProviderExternalAsUser(Object am, String name, IBinder token,
                                                            int userId) {
         try {
-            Method method = android.app.ActivityManager.class
+            Method method = Class.forName("android.app.IActivityManager")
                     .getDeclaredMethod("removeContentProviderExternalAsUser",
                             String.class, IBinder.class, int.class);
             method.invoke(am, name, token, userId);
